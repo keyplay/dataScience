9f85cc4ece19e28a8b30f100ff4c26ee8bff214e
keyplay <houyubo1993@foxmail.com>
2016-12-29 13:59:31 +0800

    Create dataclean.py

---
+++ Titanic- Machine Learning from Disaster/dataclean.py
@@ -0,0 +1,46 @@
+#! python2.7
+# -*- coding: utf-8 -*-
+# dataclean.py - clean the data  and map our features into numerical values.
+
+import pandas as pd
+
+# read data from files
+train = pd.read_csv('trainanalyse.csv')
+test = pd.read_csv('testanalyse.csv')
+full_data = [train, test]
+
+for dataset in full_data:
+    # Mapping Sex
+    dataset['Sex'] = dataset['Sex'].map( {'female': 0, 'male': 1} ).astype(int)
+    
+    # Mapping titles
+    title_mapping = {"Mr": 1, "Miss": 2, "Mrs": 3, "Master": 4, "Rare": 5}
+    dataset['Title'] = dataset['Title'].map(title_mapping)
+    dataset['Title'] = dataset['Title'].fillna(0)
+    
+    # Mapping Embarked
+    dataset['Embarked'] = dataset['Embarked'].map( {'S': 0, 'C': 1, 'Q': 2} ).astype(int)
+    
+    # Mapping Fare
+    dataset.loc[ dataset['Fare'] <= 7.91, 'Fare'] = 0
+    dataset.loc[(dataset['Fare'] > 7.91) & (dataset['Fare'] <= 14.454), 'Fare'] = 1
+    dataset.loc[(dataset['Fare'] > 14.454) & (dataset['Fare'] <= 31), 'Fare'] = 2
+    dataset.loc[ dataset['Fare'] > 31, 'Fare'] = 3
+    dataset['Fare'] = dataset['Fare'].astype(int)
+    
+    # Mapping Age
+    dataset.loc[ dataset['Age'] <= 16, 'Age'] = 0
+    dataset.loc[(dataset['Age'] > 16) & (dataset['Age'] <= 32), 'Age'] = 1
+    dataset.loc[(dataset['Age'] > 32) & (dataset['Age'] <= 48), 'Age'] = 2
+    dataset.loc[(dataset['Age'] > 48) & (dataset['Age'] <= 64), 'Age'] = 3
+    dataset.loc[ dataset['Age'] > 64, 'Age'] = 4
+
+# Feature Selection
+drop_elements = ['PassengerId', 'Name', 'Ticket', 'Cabin', 'SibSp', 'Parch', 'FamilySize']
+train = train.drop(drop_elements, axis = 1)
+train = train.drop(['CategoricalAge', 'CategoricalFare'], axis = 1)
+
+test  = test.drop(drop_elements, axis = 1)
+
+train.to_csv('trainclean.csv', index=False)
+test.to_csv('testclean.csv', index=False)
